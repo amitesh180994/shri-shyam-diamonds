08ee8e0c138467e25a5f304fde72ca6e4dc1b187
amitesh180994 <SINGHAMITESH18@GMAIL.COM>
2026-04-29 17:31:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbyqZiF-G-D8XO-_G0_t_n4qQ8TR33f2LPB7NnqRVuVc/exec";
 
 function verifyProduct() {
-  let code = document.getElementById("codeInput").value.trim().toUpperCase();
-  let result = document.getElementById("result");
+  var code = document.getElementById("codeInput").value.trim().toUpperCase();
+  var result = document.getElementById("result");
 
   result.innerHTML = "Checking...";
 
   window.showResult = function(data) {
     if (data.status === "found") {
-      result.innerHTML = `
-        <h3 style="color:green;">✅ Authentic</h3>
-        <p><b>Product:</b> ${data.product}</p>
-        <p><b>Diamond:</b> ${data.diamond}</p>
-        <p><b>Gold:</b> ${data.gold}</p>
-        <p><b>Colour:</b> ${data.colour}</p>
-        <p><b>Clarity:</b> ${data.clarity}</p>
-        <p><b>Date:</b> ${data.date}</p>
-        ${data.image ? `<img src="${data.image}" class="result-img">` : ""}
-      `;
+      var imgHtml = "";
+
+      if (data.image && data.image !== "{}") {
+        imgHtml = '<img src="' + data.image + '" class="result-img">';
+      }
+
+      result.innerHTML =
+        '<h3 style="color:green;">✅ Authentic</h3>' +
+        '<p><b>Product:</b> ' + data.product + '</p>' +
+        '<p><b>Diamond:</b> ' + data.diamond + '</p>' +
+        '<p><b>Gold:</b> ' + data.gold + '</p>' +
+        '<p><b>Colour:</b> ' + data.colour + '</p>' +
+        '<p><b>Clarity:</b> ' + data.clarity + '</p>' +
+        '<p><b>Date:</b> ' + data.date + '</p>' +
+        imgHtml;
     } else {
       result.innerHTML = "<h3 style='color:red;'>❌ Invalid Product</h3>";
     }
   };
 
-  let oldScript = document.getElementById("jsonp-script");
-  if (oldScript) oldScript.remove();
+  var old = document.getElementById("jsonp-script");
+  if (old) old.remove();
 
-  let script = document.createElement("script");
+  var script = document.createElement("script");
   script.id = "jsonp-script";
   script.src = API_URL + "?code=" + encodeURIComponent(code) + "&callback=showResult";
   document.body.appendChild(script);
